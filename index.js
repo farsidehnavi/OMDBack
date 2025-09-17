703f2ef99f3323173a450d98bd4c465ee784563d
farsidehnavi <154635498+farsidehnavi@users.noreply.github.com>
2025-09-17 12:13:56 +0330

    1

--- index.js
+++ index.js
@@ -360,7 +360,7 @@ app.post("/api/AddProfile", async (req, res) => {
 const LinkRequest = async (uuid, User, res) => {
   const options = {
     method: "GET",
-    url: HiddifyBaseUrl + "/XsaTUPj2mABSXD3LgO/api/v2/user/all-configs/",
+    url: HiddifyBaseUrl + "/Im8espfFQ0FfThFF4y/api/v2/user/all-configs/",
     headers: {
       Accept: "application/json",
       "Hiddify-API-Key": uuid,
